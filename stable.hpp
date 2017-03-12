49114ef83fdf8bd57a99f3d8139a81747879ba42
Alex-Tsvetanov <14363901+Alex-Tsvetanov@users.noreply.github.com>
2017-03-12 14:35:38 +0200

    separate constexpr and non-constexpt versions

--- stable.hpp
+++ stable.hpp
@@ -4,49 +4,6 @@ namespace Math
 	{
 		namespace Modules
 		{
-			namespace ConstExpr
-			{
-				template <typename T> constexpr T pow (T n, T k, T mod = 1e9 + 7)
-				{
-					return (k == 0) 
-									?
-										1
-									:
-										(k == 1)
-											?
-												n % mod
-											:
-												(k % 2 == 0)
-												?
-												(
-														(
-																pow (n, k / 2) 
-															* 
-																pow (n, k / 2)
-														) 
-													% 
-														mod
-												)
-												:
-												(
-														(
-																(n % mod) 
-															* 
-																(
-																		(
-																				pow (n, k / 2) 
-																			* 
-																				pow (n, k / 2)
-																		) 
-																	% 
-																		mod
-																)
-														) 
-													% 
-														mod
-												);
-				}
-			}
 			namespace Stable
 			{
 				template <typename T> T pow (T n, T k, T mod = 1e9 + 7)
@@ -88,6 +45,30 @@ namespace Math
 													% 
 														mod
 												);
+				}
+				template <typename T> T fact (T n, T mod = 1e9 + 7)
+				{
+					return (n <= 1) 
+						? 
+							1
+						: 
+							(
+									(
+											(
+													fact (n - 1) 
+												% 
+													mod
+											) 
+										* 
+											(
+													n 
+												% 
+													mod
+											)
+									) 
+								% 
+									mod
+							);
 				}
 			}
 		}
